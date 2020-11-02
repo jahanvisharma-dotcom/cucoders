fe432e7d43632203dd034190e2217149002b1fbf
Ankit Raj <ankit.raj928@gmail.com>
2020-11-02 22:59:05 +0530

    Update thanks.js

--- src/components/events/thanks.js
+++ src/components/events/thanks.js
@@ -49,8 +49,8 @@ export default ({
   imageBorder = false,
   imageShadow = true,
   subheading = "We're so glad you registered for the event.",
-  heading = "Nov. 01st, at 22:00 IST",
-  description = "Thank you for registering for the CU-Coders Online Challenge. You have registered for QUIZ1: CU-Coders Online Challenge(Quiz)",
+  heading = "Nov. 03rd, at 22:00 IST",
+  description = "Thank you for registering for the CU-Coders Online Challenge. You have registered for QUIZ2: CU-Coders Online Challenge(Quiz)",
   textOnLeft = false,
   testimonials = [
     {
