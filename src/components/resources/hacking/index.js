66b52c191147f20273f3d123efc478d9dc7bc463
Ankit Raj <ankit.raj928@gmail.com>
2021-02-27 23:46:22 +0530

    Update index.js

--- src/components/resources/hacking/index.js
+++ src/components/resources/hacking/index.js
@@ -51,9 +51,9 @@ export default ({
   posts = [
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&q=80",
-      category: "Travel Tips",
-      date: "April 21, 2020",
+        "https://images.unsplash.com/photo-1592772874383-d08932d29db7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&q=80",
+      category: "Book",
+      date: "Feb 25, 2021",
       title: "Introduction to Microsoft Active Directory Domain Services(AD DS)",
       description:
         "",
