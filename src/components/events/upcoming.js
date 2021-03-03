e79dc7d84ea4e9fdd99934c7d476d7a7d744e726
Ankit Raj <ankit.raj928@gmail.com>
2021-03-03 23:30:41 +0530

    Add files via upload

--- src/components/events/upcoming.js
+++ src/components/events/upcoming.js
@@ -46,7 +46,7 @@ export default () => {
   const cards = [
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1612286648571-5cde43a710ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+        "https://images.unsplash.com/photo-1606326608690-4e0281b1e588?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
       subtitle: "Contest",
       title: "Quiz Mania",
       description:
