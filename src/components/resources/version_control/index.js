0dbbc30e7b0e89cae53fe883f9ea507506e26423
Ankit Raj <ankit.raj928@gmail.com>
2021-07-09 09:48:53 +0530

    Update index.js

--- src/components/resources/version_control/index.js
+++ src/components/resources/version_control/index.js
@@ -47,7 +47,7 @@ const ButtonContainer = tw.div`flex justify-center`;
 const LoadMoreButton = tw(PrimaryButton)`mt-16 mx-auto`;
 
 export default ({
-  headingText = "Security Resources",
+  headingText = "Version Control Resources",
   posts = [
     {
       imageSrc:
